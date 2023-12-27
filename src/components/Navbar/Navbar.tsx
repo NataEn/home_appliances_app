3d80a58c13d6f1f432ff685ca75601d9ec6e6268
Natalie Enkin <natalie@enkin.co.il>
2023-12-27 12:56:47 +0200

    app init

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+const Navbar: FC =() => {
+return (<div className="navbar container">
+    <a href="#!"> Home Appliances Hub</a>
+    <div className="nav-links">
+        <a href="#!">Home</a>
+        <a href="#!">Designs</a>
+        <a href="#!">Settings</a>
+    </div>
+    </div>)
+}
+
+export default Navbar
