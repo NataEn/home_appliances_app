695acd9a05e07ff3bf577325e9a3c0f848e41d4c
Natalie Enkin <natalie@enkin.co.il>
2024-01-07 15:20:16 +0200

    app file structure

---
+++ src/pages/homePage/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+import Hero from '../components/Hero/Hero'
+
+const Home :FC=()=>{
+    return <div>
+        Home page
+        <Hero/>
+    </div>
+}
+
+export default Home;
---
+++ src/pages/homePage/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const Hero :FC=()=>{
+    return <div>
+        hero section
+    </div>
+}
+
+export default Hero;
