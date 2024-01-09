fc26e98626094d66ff2d1dd4e36f8629f9180226
Natalie Enkin <natalie@enkin.co.il>
2024-01-09 12:04:04 +0200

    info of Hero section

--- src/pages/homePage/components/Hero/Hero.tsx
+++ src/pages/homePage/components/Hero/Hero.tsx
@@ -1,8 +1,18 @@
 import React, { FC } from 'react'
 
 const Hero :FC=()=>{
-    return <div>
-        hero section
+    return <div className='hero'>
+        <section>
+        <div className='col'>
+            <h1 className='title'>What Are We About</h1>
+            <p className='info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            incididunt ut duis aute irure.</p>
+            <button className='btn'>explore now</button>
+        </div>
+        <div className='col'>col2</div>
+
+        </section>
     </div>
 }
 
