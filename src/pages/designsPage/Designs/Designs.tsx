70f4ac1e8ecb45488503fe15899cdb62fa6577cf
Natalie Enkin <natalie@enkin.co.il>
2024-01-24 12:14:19 +0200

    designs page init

--- src/pages/designsPage/Designs/Designs.tsx
+++ src/pages/designsPage/Designs/Designs.tsx
@@ -1,5 +1,9 @@
 import React, {FC} from "react";
+import PrevSearches from "./../components/PreviousSearches/PreviosSearches"
 
 export default function Designs(){
-    return <div>Designs</div>
+    return <div className="designs">
+        <PrevSearches/>
+        
+        Designs</div>
 }
---
+++ src/pages/designsPage/components/PreviousSearches/PreviosSearches.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+
+const PreviousSearches: FC=(): JSX.Element=>{
+    return <div className="prevSearches">
+        <h2>Previous Searches</h2>
+    </div>
+}
+
+export default PreviousSearches
