b4da6b13b66dcc611e8479fc74d8a4c5b68eece7
Natalie Enkin <natalie@enkin.co.il>
2024-01-24 12:14:00 +0200

    improve tests

--- src/Utils/mainUtils.test.tsx
+++ src/Utils/mainUtils.test.tsx
@@ -2,39 +2,23 @@ import * as mainUtils from "./mainUtils"
 
 describe('AddClassNames function', () => {
     it('returns empty string for empty string', () => {
-        const classNames_1 : mainUtils.AddClassNamesParam=""
-        expect(mainUtils.AddClassNames(classNames_1)).toBe("")
-    });
-
-    it('returns empty string for a number', () => {
-        const classNames_2 : mainUtils.AddClassNamesParam=2
-        expect(mainUtils.AddClassNames(classNames_2)).toBe("")
-    });
-
-    it('returns empty string for a boolean', () => {
-        const classNames_3 : mainUtils.AddClassNamesParam=true
-        expect(mainUtils.AddClassNames(classNames_3)).toBe("")
-    });
-
-    it('returns empty string for a function', () => {
-        const emptyFunction=()=>false
-        const classNames_4 : mainUtils.AddClassNamesParam=emptyFunction
-        expect(mainUtils.AddClassNames(classNames_4)).toBe("")
+        const className : mainUtils.AddClassNamesParam=""
+        expect(mainUtils.AddClassNames(className)).toBe("")
     });
     
     it('returns empty string for one class name', () => {
-        const classNames_6 : mainUtils.AddClassNamesParam="test"
-        expect(mainUtils.AddClassNames(classNames_6)).toBe("test")
+        const className : mainUtils.AddClassNamesParam="test"
+        expect(mainUtils.AddClassNames(className)).toBe("test")
     });
 
     it('returns empty string for array of strings', () => {
-        const classNames_7 : mainUtils.AddClassNamesParam=["test", "test_two"]
-        expect(mainUtils.AddClassNames(classNames_7)).toBe("test test_two")
+        const className : mainUtils.AddClassNamesParam=["test", "test_two"]
+        expect(mainUtils.AddClassNames(className)).toBe("test test_two")
     });
     
     it('returns empty string for array of strings and undefined', () => {
-        const classNames_7 : mainUtils.AddClassNamesParam=["test", undefined, "test_two"]
-        expect(mainUtils.AddClassNames(classNames_7)).toBe("test test_two")
+        const className : mainUtils.AddClassNamesParam=["test", undefined, "test_two"]
+        expect(mainUtils.AddClassNames(className)).toBe("test test_two")
     });
 
   });
--- src/Utils/mainUtils.tsx
+++ src/Utils/mainUtils.tsx
@@ -5,14 +5,11 @@ export function AddClassNames(classNames: AddClassNamesParam): string {
     if (Array.isArray(classNames)) {
         const names = classNames.filter(className => (typeof className === "string"))
         if (names.length > 1) {
-            console.log("end", names.join(" ")) 
             return names.join(" ")   
         }
     }
-    else{
         // if any other type of var is given --> return empty string
         return ""
-    }
 }
 
 module.exports = {
