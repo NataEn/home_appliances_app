9159b1c96891d09ecdde714740c5baa46694142c
Natalie Enkin <natalie@enkin.co.il>
2024-02-20 11:34:33 +0200

    updating image on card comp

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 
 interface ImageProps{
-    imgSrc: string;
-    title: string;
+    imgSrc?: string;
+    title?: string;
     index?: number | string;
 }
 
 const Image: FC<ImageProps> = ({imgSrc, title, index}) => {
         return <div className='image'>
-            <img src={process.env.PUBLIC_URL +imgSrc} alt={title + index}/>
+            <img src={process.env.PUBLIC_URL +imgSrc} alt={title && (title + index)}/>
             </div>    
 }
 
