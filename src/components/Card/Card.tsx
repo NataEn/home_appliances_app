772392709f116af9ce31f2df8542f0685cad1c72
Natalie Enkin <natalie@enkin.co.il>
2024-02-20 11:36:46 +0200

    updating info direction

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,36 @@
-import React, {FC} from "react";
-export interface CardProps{
-title: string;
-description: string | JSX.Element;
-uuid: string;
-image: {src: string, alt: string, [key: string]: any}
-} 
-const Card:FC<CardProps>=({title, description, image, uuid})=>{
-    return<div className="card" key={uuid}>
-        <div className="card-img">
-            <img src={image.src} alt={image.alt}/>
-        </div>
-        <div className="card-info">
-            <h3 className="card-info-title">{title}</h3>
-            <p className="card-info-text">{description}</p>
-        </div>
-    </div>
+import React, { FC } from "react";
+import * as Utils from "../../Utils/mainUtils"
+import Image from "../Image/Image";
+export interface CardProps {
+    title?: string;
+    description: string | JSX.Element;
+    uuid: string | number;
+    image?: { src: string, alt: string, [key: string]: any }
+    className?: string;
+    infoDirection?: "right" | "left" | "up" | "down";
+}
+const Card: FC<CardProps> = ({ title, description, image, uuid, className, infoDirection = "right" }) => {
+    const directionClass = (direction: String) => {
+        switch (direction) {
+            case "left": return "infoLeft"
+            case "right": return "infoRight"
+            case "up": return "infoUp"
+            case "down": return "infoDown"
+            default: return "infoRight"
+        }
+    }
+    return (
+        <div
+            className={Utils.AddClassNames(["card", className, directionClass(infoDirection)])}
+            key={uuid}>
+            <div className="card-img">
+                {image && <img src={image.src} alt={image.alt} />}
+            </div>
+            <div className="card-info">
+                {title && <h3 className="card-info-title" >{title}</h3>}
+                <p className="card-info-text">{description}</p>
+            </div>
+        </div>)
 }
 
 export default Card;
