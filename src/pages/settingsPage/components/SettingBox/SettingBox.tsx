b02fd23b74d384a07ceff846c337cdaf277637b5
Natalie Enkin <natalie@enkin.co.il>
2024-03-11 13:02:49 +0200

    typing issues

--- src/pages/settingsPage/components/SettingBox/SettingBox.tsx
+++ src/pages/settingsPage/components/SettingBox/SettingBox.tsx
@@ -19,17 +19,18 @@ const SettingBox: FC<SettingBoxProps> = ({ key, title, options, className }): JS
 
     const updateSettingsContext=(title: string, value: string)=>{
           const newSettings={...settings, [title]:value}
+          console.log(`updating to ${title}:${value}`)
           setSettings({...newSettings})
     }
-    const onClickSettingOption = (className: string, value: string): void => {
+    const onClickSettingOption = (className: string, value: string, title: string): void => {
         console.log("from setting box", className)
         setSettingOption(className)
-        updateSettingsContext(className, value)
+        updateSettingsContext(title, value)
     }
 
     return <div className={Utils.AddClassNames(["setting-box", className])} key={key}>
         <h2>{title}</h2>
-        <div className="options-container">{options.map((option: StyleSettingOption) => <SettingOption onClickOption={onClickSettingOption} selectedOption={settingOption} uuid={option.uuid} value={option.value} className={option.className} />)}</div>
+        <div className="options-container">{options.map((option: StyleSettingOption) => <SettingOption onClickOption={onClickSettingOption} selectedOption={settingOption} uuid={option.uuid} value={option.value} className={option.className} title={title} />)}</div>
     </div>
 }
 
--- src/pages/settingsPage/components/SettingOption/SettingOption.tsx
+++ src/pages/settingsPage/components/SettingOption/SettingOption.tsx
@@ -8,12 +8,13 @@ export interface SettingOptionProps {
     selectedOption: string,
     value: string,
     className: string,
-    onClickOption:(optionClassName:string, value:string)=>void
+    title: string,
+    onClickOption:(className: string, value: string, title: string)=>void
 }
 // const styleClassName = (value: string, className: string): string => value ? "btn" : `${className} regular`;
 
-const SettingOption: FC<SettingOptionProps> = ({ uuid, value, className, selectedOption, onClickOption }): JSX.Element => {
-    return <div className={Utils.AddClassNames(["option", "btn", className])} key={uuid} onClick={(e: React.MouseEvent)=>onClickOption(className, value)}>
+const SettingOption: FC<SettingOptionProps> = ({ uuid, value, className, title, selectedOption, onClickOption }): JSX.Element => {
+    return <div className={Utils.AddClassNames(["option", "btn", className])} key={uuid} onClick={(e: React.MouseEvent)=>onClickOption(className, value, title)}>
         {value&&<span className="option-value">{value}</span>}
     {selectedOption===className && <div className="check"><FontAwesomeIcon icon={faCheck}/></div>}
 </div>
